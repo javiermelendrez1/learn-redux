bd1688a3e005ba09a7e0011e35c229747cb0a0f0
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2022-01-13 12:34:12 -0800

    updated app.js
    - posts will render on home page

--- redux-essentials-example-app-master/src/app/store.js
+++ redux-essentials-example-app-master/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import postsReducer from '../features/posts/postsSlice'
+import postsReducer from '../features/posts/postSlice'
 //import post reducer
 //have a field called posts, posts will be updated by reducer 
 
