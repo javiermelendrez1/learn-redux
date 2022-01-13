fda9cc22a19bd35eebd2f69a29c9b018a6cebe42
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2022-01-13 12:37:17 -0800

    created AddPostForm.js
    - will use this component to create new posts

---
+++ redux-essentials-example-app-master/src/features/posts/AddPostForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+export const AddPostForm = () => {
+    //create states 
+    //these states are not placed in redux store because
+    //they are not used outside of this component
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const onTitleChanged = e => setTitle(e.target.value)
+  const onContentChanged = e => setContent(e.target.value)
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button">Save Post</button>
+      </form>
+    </section>
+  )
+}
