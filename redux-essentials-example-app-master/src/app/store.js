640250811a2cb4a6558515345bd577234fafb601
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2022-01-19 11:46:45 -0800

    created slice for users

--- redux-essentials-example-app-master/src/app/store.js
+++ redux-essentials-example-app-master/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postSlice'
+import usersReducer from '../features/users/usersSlice'
 //import post reducer
 //have a field called posts, posts will be updated by reducer 
 
 export default configureStore({
   reducer: {
-    posts: postsReducer
+    posts: postsReducer,
+    users: usersReducer
   }
 })
